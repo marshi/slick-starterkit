b78f6f629fb34add0af566571821512405e1f4e8
marshi <mashi3648@gmail.com>
2015-07-04 23:46:21 +0900

    サンプル追加

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -17,28 +17,45 @@ object App {
 
   def main(args: Array[String]): Unit = {
     select
-    update
-    insert(30)
-    delete(16)
+    insertHost(30, "test01")
+    insertBranch(30, "test01-branch01")
+    selectJoin
+    update(30, "test01-update")
+    deleteHost(30)
+    deleteBranch(30)
   }
 
-  def insert(id: Int) = {
-    val q = Tables.HostMachine.map(h => (h.name)) += (Some("test_name"))
+  def insertHost(id: Int, hostName: String) = {
+    // forceInsertではauto incrementなカラムであっても指定した値を挿入することができる.
+    val q = Tables.HostMachine.map(h => (h.id, h.name)).forceInsert(id, Some(hostName))
     val future = db.run(q)
     Await.ready(future, Duration.Inf)
   }
 
-  def delete(id: Int) = {
+  def insertBranch(hostId: Int, branchName: String) = {
+    // insertは += を使うことも可能.
+    val q = Tables.HostBranch.map(b => (b.hostMachineId, b.branchName)) += (Some(hostId), Some(branchName))
+    val future = db.run(q)
+    Await.ready(future, Duration.Inf)
+  }
+
+  def deleteHost(id: Int) = {
     val q = Tables.HostMachine.filter(_.id === id).delete
     val future = db.run(q)
     Await.ready(future, Duration.Inf)
   }
+  
+  def deleteBranch(hostId: Int) = {
+    val q = Tables.HostBranch.filter(_.hostMachineId === hostId).delete
+    val future = db.run(q)
+    Await.ready(future, Duration.Inf)
+  }
 
-  def update = {
+  def update(id: Int, hostName: String) = {
     // filter ≒ where.
     // map(m => m.◯◯) ≒ "set ◯◯ = "
     // update(□□) ≒ (set ◯◯ =) □□
-    val q = Tables.HostMachine.filter(_.id === 3).map(h => (h.id, h.name)).update((3, Some("dev-pc03")))
+    val q = Tables.HostMachine.filter(_.id === 3).map(h => (h.id, h.name)).update((id, Some("dev-pc03")))
     val future = db.run(q)
     Await.ready(future, Duration.Inf)
   }
@@ -53,4 +70,19 @@ object App {
     }
     Await.ready(future, Duration.Inf)
   }
+
+  /**
+   * host_machineテーブルのIDとhost_branchテーブルのhost_machine_idが一致するレコードをselectする
+   * @return
+   */
+  def selectJoin = {
+    val q = Tables.HostMachine.join(Tables.HostBranch).on(_.id === _.hostMachineId).result
+    val future = db.run(q)
+    future.onComplete {
+      case Failure(_) => Nil
+      case Success(r) => r.foreach(t => println(t._1.name + " " + t._2.branchName))
+    }
+    Await.ready(future, Duration.Inf)
+  }
+
 }
